805227e0f3d80d99a09a26b509f7d519de2135d7
LenaBirrr <lenabir68@gmail.com>
2022-10-10 14:51:07 +0300

    Correct

--- SOA3/Program.cs
+++ SOA3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SOA3;
 
 CalculationSaver calculationSaver = new CalculationSaver();
-int Calc(int a, int b) => a + b;
+int Add(int a, int b) => a + b;
+int Sub(int a, int b) => a - b;
 
-Console.WriteLine(calculationSaver.CalculateAndSave(Calc, 3, 2));
+//Console.WriteLine(calculationSaver.CalculateAndSave(Add, 3, 2));
+Console.WriteLine(calculationSaver.CalculateAndSave(Sub, 3, 2));
