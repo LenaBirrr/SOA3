ca8898d7b9fe551b324f00647640843f23413741
LenaBirrr <lenabir68@gmail.com>
2022-10-14 11:57:16 +0300

    fixed

--- SOA3/Program.cs
+++ SOA3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using SOA3;
 
 CalculationSaver calculationSaver = new CalculationSaver();
-CalculationSaver.Operation operation = CalculationSaver.Operation.Sub;
-Console.WriteLine(calculationSaver.CalculateAndSave(3, 2, operation));
+Console.WriteLine(calculationSaver.CalculateAndSave(3, 2, CalculationSaver.Operation.Sub));
